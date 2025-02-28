a50cd6e6b6a3a7fdbe0384ab2b4b6c62bb1521af
Sally McGrath <sally@codeyourfuture.io>
2025-02-27 19:20:40 -0500

    rejig script, add tags for subtopics

--- assets/scripts/filter.js
+++ assets/scripts/filter.js
@@ -1,58 +1,53 @@
-function signpostFilter(topicsByRegion) {
+function signpostFilter() {
   const form = document.querySelector("form");
   const regionInput = document.getElementById("region");
   const topicInput = document.getElementById("topic");
-  const sections = document.querySelectorAll("section[data-regions]");
-  const accordionItems = document.querySelectorAll("details");
+  const article = document.getElementById("signposts-by-region");
+  const sections = article.querySelectorAll("section[data-regions]");
+  const entries = article.querySelectorAll("details");
 
-  const updateVisibility = (element, isVisible) =>
-    element.toggleAttribute("hidden", !isVisible);
+  const normalise = (value) =>
+    value ? value.toLowerCase().replace(/ /g, "-") : "";
+  const hideAll = () => {
+    sections.forEach((section) => (section.hidden = true));
+    entries.forEach((entry) => (entry.hidden = true));
+  };
 
-  const hideAll = (isFormPresent) => {
-    if (!isFormPresent) {
-      return;
+  const filter = () => {
+    hideAll();
+    const selectedRegion = normalise(regionInput.value) || false;
+    const selectedTopic = normalise(topicInput.value) || false;
+
+    // If no region selected, do nothing
+    if (!selectedRegion) return;
+
+    // When I select a region, then show that datalist (by choosing the list in the html, go look)
+    topicInput.setAttribute("list", selectedRegion);
+
+    if (selectedRegion) {
+      // Find and show the current section
+      const currentSection = [...sections].find(
+        (section) => normalise(section.dataset.regions) === selectedRegion
+      );
+      currentSection.hidden = false;
+
+      // Handle entries within the current section
+      const sectionEntries = currentSection.querySelectorAll("details");
+      sectionEntries.forEach((detail) => {
+        detail.hidden =
+          selectedTopic && normalise(detail.dataset.topics) !== selectedTopic;
+      });
     }
-    sections.forEach((section) => updateVisibility(section, false));
-    accordionItems.forEach((item) => updateVisibility(item, false));
   };
 
-  const filterSectionsAndAccordionItems = (selectedRegion, selectedTopic) => {
-    sections.forEach((section) => {
-      const sectionRegions = section.dataset.regions
-        .split(" | ")
-        .map((region) => region.toLowerCase().replace(/ /g, "-"));
+  hideAll();
 
-      const isVisible = sectionRegions.includes(selectedRegion);
-      updateVisibility(section, isVisible);
-
-      if (isVisible) {
-        const items = section.querySelectorAll("details");
-        items.forEach((item) => {
-          const itemTopics = item.dataset.topics.split(" | ");
-          const itemVisible = itemTopics.includes(selectedTopic);
-          updateVisibility(item, itemVisible);
-        });
-      }
-    });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const selectedRegion = regionInput.value.toLowerCase().replace(/ /g, "-");
-    const selectedTopic = topicInput.value;
-
-    filterSectionsAndAccordionItems(selectedRegion, selectedTopic);
-  };
-
-  const handleRegionChange = () => {
-    const selectedRegion = regionInput.value.toLowerCase().replace(" ", "-");
-    topicInput.setAttribute("list", `${selectedRegion}`);
-  };
-
-  hideAll(form);
-  regionInput.addEventListener("input", handleRegionChange);
-  form.addEventListener("submit", handleSubmit);
+  regionInput.addEventListener("input", filter);
+  topicInput.addEventListener("input", filter);
+  form.addEventListener("reset", () => {
+    hideAll();
+    topicInput.setAttribute("list", " ");
+  });
 }
 
 document.addEventListener("DOMContentLoaded", signpostFilter);
